f92df4d9d89e13ddc4ebdf283d479576deefeab6
PfaulJulian <pfaul.julian@gmail.com>
2025-01-25 15:53:28 +0100

    changed signatur of function allocator_create to (allocate_func_t, void* alloc_args, deallocate_func_t, void* dealloc_args)

--- include/julian_pfaul/allocator.h
+++ include/julian_pfaul/allocator.h
@@ -1,15 +1,18 @@
 #pragma once
 #include <stddef.h>
 
-typedef typeof(void*(*)(size_t)) allocate_func_t;
-typedef typeof(void (*)(void*))  deallocate_func_t;
+typedef typeof(void*(*)(void* args, size_t sz)) allocate_func_t;
+typedef typeof(void (*)(void* args, void* ptr)) deallocate_func_t;
 
 struct allocator {
+    void*             allocate_args;
+    void*             deallocate_args;
     allocate_func_t   allocate;
     deallocate_func_t deallocate;
 };
 
-struct allocator allocator_create(allocate_func_t, deallocate_func_t);
+struct allocator allocator_create(allocate_func_t, void* alloc_args, deallocate_func_t, void* dealloc_args);
 
 void* allocator_allocate(struct allocator, size_t);
+
 void allocator_deallocate(struct allocator, void*);
--- source/allocator.c
+++ source/allocator.c
@@ -1,16 +1,19 @@
 #include <julian_pfaul/allocator.h>
 
-struct allocator allocator_create(const allocate_func_t alloc_func, const deallocate_func_t dealloc_func) {
+struct allocator allocator_create(const allocate_func_t   alloc_func, void*   alloc_args,
+                                  const deallocate_func_t dealloc_func, void* dealloc_args) {
     struct allocator allocator;
+    allocator.allocate_args = alloc_args;
+    allocator.deallocate_args = dealloc_args;
     allocator.allocate   = alloc_func;
     allocator.deallocate = dealloc_func;
     return allocator;
 }
 
 void* allocator_allocate(struct allocator allocator, size_t size) {
-    return allocator.allocate(size);
+    return allocator.allocate(allocator.allocate_args, size);
 }
 
 void allocator_deallocate(struct allocator allocator, void* ptr) {
-    allocator.deallocate(ptr);
+    allocator.deallocate(allocator.deallocate_args, ptr);
 }
