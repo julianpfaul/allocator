08adc09f78b33a094c9858990f347a793006f88a
PfaulJulian <pfaul.julian@gmail.com>
2025-01-27 17:51:35 +0100

    reworked everything

--- include/julian_pfaul/allocator.h
+++ include/julian_pfaul/allocator.h
@@ -1,18 +1,73 @@
 #pragma once
+
 #include <stddef.h>
 
-typedef typeof(void*(*)(void* args, size_t sz)) allocate_func_t;
-typedef typeof(void (*)(void* args, void* ptr)) deallocate_func_t;
+typedef typeof(void* (*)(size_t sz))                          allocate_func_t;
+typedef typeof(void* (*)(void* state, size_t sz))             allocate_func_with_state_t;
+typedef typeof(void* (*)(size_t sz, void* args))              allocate_func_extended_t;
+typedef typeof(void* (*)(void* state, size_t sz, void* args)) allocate_func_extended_with_state_t;
 
-struct allocator {
-    void*             allocate_args;
-    void*             deallocate_args;
-    allocate_func_t   allocate;
-    deallocate_func_t deallocate;
+typedef typeof(void (*)(void* ptr))                          deallocate_func_t;
+typedef typeof(void (*)(void* state, void* ptr))             deallocate_func_with_state_t;
+typedef typeof(void (*)(void* ptr, void* args))              deallocate_func_extended_t;
+typedef typeof(void (*)(void* state, void* ptr, void* args)) deallocate_func_extended_with_state_t;
+
+enum allocator_function_type : unsigned char {
+    ALLOCATOR_FUNCTION_SIMPLE              = 0,
+    ALLOCATOR_FUNCTION_WITH_STATE          = 1,
+    ALLOCATOR_FUNCTION_EXTENDED            = 2,
+    ALLOCATOR_FUNCTION_EXTENDED_WITH_STATE = 3
 };
 
-struct allocator allocator_create(allocate_func_t, void* alloc_args, deallocate_func_t, void* dealloc_args);
+struct allocate_function {
+    enum allocator_function_type type;
+    void*                        state;
 
-void* allocator_allocate(struct allocator, size_t);
+    union {
+        allocate_func_t                     simple;
+        allocate_func_with_state_t          with_state;
+        allocate_func_extended_t            extended;
+        allocate_func_extended_with_state_t extended_with_state;
+    };
+};
 
-void allocator_deallocate(struct allocator, void*);
+struct allocate_function allocate_function_create_simple(allocate_func_t func);
+struct allocate_function allocate_function_create_with_state(allocate_func_with_state_t func, void* state);
+struct allocate_function allocate_function_create_extended(allocate_func_extended_t func);
+struct allocate_function allocate_function_create_extended_with_state(allocate_func_extended_with_state_t func, void* state);
+
+void* allocate_function_call(struct allocate_function, size_t sz, void* extended_args);
+
+struct deallocate_function {
+    enum allocator_function_type type;
+    void*                        state;
+
+    union {
+        deallocate_func_t                     simple;
+        deallocate_func_with_state_t          with_state;
+        deallocate_func_extended_t            extended;
+        deallocate_func_extended_with_state_t extended_with_state;
+    };
+};
+
+struct deallocate_function deallocate_function_create_simple(deallocate_func_t func);
+struct deallocate_function deallocate_function_create_with_state(deallocate_func_with_state_t func, void* state);
+struct deallocate_function deallocate_function_create_extended(deallocate_func_extended_t func);
+struct deallocate_function deallocate_function_create_extended_with_state(deallocate_func_extended_with_state_t func, void* state);
+
+void deallocate_function_call(struct deallocate_function, void* ptr, void* extended_args);
+
+struct allocator {
+    struct allocate_function   allocate;
+    struct deallocate_function deallocate;
+};
+
+struct allocator allocator_create(allocate_func_t allocate_func, deallocate_func_t deallocate_func);
+struct allocator allocator_create_with_state(allocate_func_with_state_t allocate_func, deallocate_func_with_state_t deallocate_func, void* state);
+struct allocator allocator_create_extended(allocate_func_extended_t allocate_func, deallocate_func_extended_t deallocate_func);
+struct allocator allocator_create_extended_with_state(allocate_func_extended_with_state_t allocate_func, deallocate_func_extended_with_state_t deallocate_func, void* state);
+
+struct allocator allocator_create_custom(struct allocate_function, struct deallocate_function);
+
+void* allocator_allocate(struct allocator, size_t sz, void* extended_args);
+void  allocator_deallocate(struct allocator, void* ptr, void* extended_args);
--- source/allocator.c
+++ source/allocator.c
@@ -1,19 +1,153 @@
 #include <julian_pfaul/allocator.h>
 
-struct allocator allocator_create(const allocate_func_t   alloc_func, void*   alloc_args,
-                                  const deallocate_func_t dealloc_func, void* dealloc_args) {
+// ALLOCATE
+
+struct allocate_function allocate_function_create_simple(allocate_func_t func) {
+    struct allocate_function allocate_function;
+    allocate_function.type   = ALLOCATOR_FUNCTION_SIMPLE;
+    allocate_function.state  = NULL;
+    allocate_function.simple = func;
+
+    return allocate_function;
+}
+
+struct allocate_function allocate_function_create_with_state(allocate_func_with_state_t func, void* state) {
+    struct allocate_function allocate_function;
+    allocate_function.type       = ALLOCATOR_FUNCTION_WITH_STATE;
+    allocate_function.state      = state;
+    allocate_function.with_state = func;
+
+    return allocate_function;
+}
+
+struct allocate_function allocate_function_create_extended(allocate_func_extended_t func) {
+    struct allocate_function allocate_function;
+    allocate_function.type     = ALLOCATOR_FUNCTION_EXTENDED;
+    allocate_function.state    = NULL;
+    allocate_function.extended = func;
+
+    return allocate_function;
+}
+
+struct allocate_function allocate_function_create_extended_with_state(allocate_func_extended_with_state_t func, void* state) {
+    struct allocate_function allocate_function;
+    allocate_function.type                = ALLOCATOR_FUNCTION_EXTENDED_WITH_STATE;
+    allocate_function.state               = state;
+    allocate_function.extended_with_state = func;
+
+    return allocate_function;
+}
+
+// DEALLOCATE
+
+struct deallocate_function deallocate_function_create_simple(deallocate_func_t func) {
+    struct deallocate_function deallocate_function;
+    deallocate_function.type   = ALLOCATOR_FUNCTION_SIMPLE;
+    deallocate_function.state  = NULL;
+    deallocate_function.simple = func;
+
+    return deallocate_function;
+}
+
+struct deallocate_function deallocate_function_create_with_state(deallocate_func_with_state_t func, void* state) {
+    struct deallocate_function deallocate_function;
+    deallocate_function.type       = ALLOCATOR_FUNCTION_WITH_STATE;
+    deallocate_function.state      = state;
+    deallocate_function.with_state = func;
+
+    return deallocate_function;
+}
+
+struct deallocate_function deallocate_function_create_extended(deallocate_func_extended_t func) {
+    struct deallocate_function deallocate_function;
+    deallocate_function.type     = ALLOCATOR_FUNCTION_EXTENDED;
+    deallocate_function.state    = NULL;
+    deallocate_function.extended = func;
+
+    return deallocate_function;
+}
+
+struct deallocate_function deallocate_function_create_extended_with_state(deallocate_func_extended_with_state_t func, void* state) {
+    struct deallocate_function deallocate_function;
+    deallocate_function.type                = ALLOCATOR_FUNCTION_EXTENDED_WITH_STATE;
+    deallocate_function.state               = state;
+    deallocate_function.extended_with_state = func;
+
+    return deallocate_function;
+}
+
+struct allocator allocator_create(allocate_func_t allocate_func, deallocate_func_t deallocate_func) {
     struct allocator allocator;
-    allocator.allocate_args = alloc_args;
-    allocator.deallocate_args = dealloc_args;
-    allocator.allocate   = alloc_func;
-    allocator.deallocate = dealloc_func;
+    allocator.allocate   = allocate_function_create_simple(allocate_func);
+    allocator.deallocate = deallocate_function_create_simple(deallocate_func);
+
     return allocator;
 }
 
-void* allocator_allocate(struct allocator allocator, size_t size) {
-    return allocator.allocate(allocator.allocate_args, size);
+struct allocator allocator_create_with_state(allocate_func_with_state_t allocate_func, deallocate_func_with_state_t deallocate_func, void* state) {
+    struct allocator allocator;
+    allocator.allocate   = allocate_function_create_with_state(allocate_func, state);
+    allocator.deallocate = deallocate_function_create_with_state(deallocate_func, state);
+
+    return allocator;
 }
 
-void allocator_deallocate(struct allocator allocator, void* ptr) {
-    allocator.deallocate(allocator.deallocate_args, ptr);
+struct allocator allocator_create_extended(allocate_func_extended_t allocate_func, deallocate_func_extended_t deallocate_func) {
+    struct allocator allocator;
+    allocator.allocate   = allocate_function_create_extended(allocate_func);
+    allocator.deallocate = deallocate_function_create_extended(deallocate_func);
+
+    return allocator;
+}
+
+struct allocator allocator_create_extended_with_state(allocate_func_extended_with_state_t allocate_func, deallocate_func_extended_with_state_t deallocate_func, void* state) {
+    struct allocator allocator;
+    allocator.allocate   = allocate_function_create_extended_with_state(allocate_func, state);
+    allocator.deallocate = deallocate_function_create_extended_with_state(deallocate_func, state);
+
+    return allocator;
+}
+
+struct allocator allocator_create_custom(struct allocate_function alloc, struct deallocate_function dealloc) {
+    struct allocator allocator;
+    allocator.allocate   = alloc;
+    allocator.deallocate = dealloc;
+
+    return allocator;
+}
+
+void* allocate_function_call(struct allocate_function alloc, size_t sz, void* extended_args) {
+    switch (alloc.type) {
+        case ALLOCATOR_FUNCTION_SIMPLE: return alloc.simple(sz);
+        case ALLOCATOR_FUNCTION_WITH_STATE: return alloc.with_state(alloc.state, sz);
+        case ALLOCATOR_FUNCTION_EXTENDED: return alloc.extended(sz, extended_args);
+        case ALLOCATOR_FUNCTION_EXTENDED_WITH_STATE: return alloc.extended_with_state(alloc.state, sz, extended_args);
+    }
+
+    return NULL;
+}
+
+void deallocate_function_call(struct deallocate_function dealloc, void* ptr, void* extended_args) {
+    switch (dealloc.type) {
+        case ALLOCATOR_FUNCTION_SIMPLE:
+            dealloc.simple(ptr);
+            break;
+        case ALLOCATOR_FUNCTION_WITH_STATE:
+            dealloc.with_state(dealloc.state, ptr);
+            break;
+        case ALLOCATOR_FUNCTION_EXTENDED:
+            dealloc.extended(ptr, extended_args);
+            break;
+        case ALLOCATOR_FUNCTION_EXTENDED_WITH_STATE:
+            dealloc.extended_with_state(dealloc.state, ptr, extended_args);
+            break;
+    }
+}
+
+void* allocator_allocate(struct allocator allocator, size_t sz, void* extended_args) {
+    return allocate_function_call(allocator.allocate, sz, extended_args);
+}
+
+void allocator_deallocate(struct allocator allocator, void* ptr, void* extended_args) {
+    return deallocate_function_call(allocator.deallocate, ptr, extended_args);
 }
